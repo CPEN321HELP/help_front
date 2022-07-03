4121e64bfa380cddd95ee8f3b4bb1009a4195315
HizanLi <L2542293790@gmail.com>
2022-07-02 20:40:12 -0700

    update for connecting to db

--- app/src/main/java/com/example/help_m5/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/help_m5/ui/home/HomeFragment.java
@@ -1,19 +1,27 @@
 package com.example.help_m5.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.help_m5.databinding.FragmentHomeBinding;
+import com.example.help_m5.tempdb;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class HomeFragment extends Fragment {
 
+    private static final String TAG = "HomeFragment";
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -23,11 +31,15 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        String File_Name= "Demo.txt"; //gives file name
+        String Data="Hello!!"; //define data
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
+
     }
+
+
 
     @Override
     public void onDestroyView() {
