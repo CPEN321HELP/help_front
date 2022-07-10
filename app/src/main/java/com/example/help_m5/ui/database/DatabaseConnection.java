49874080f2ecb836a4f10f15eed3df2053f78f61
HizanLi <L2542293790@gmail.com>
2022-07-09 17:41:40 -0700

    update - remove facilityFragment

--- app/src/main/java/com/example/help_m5/ui/database/DatabaseConnection.java
+++ app/src/main/java/com/example/help_m5/ui/database/DatabaseConnection.java
@@ -15,9 +15,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.help_m5.databinding.FragmentFacilityBinding;
 import com.example.help_m5.databinding.FragmentReportBinding;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
 
 import org.json.JSONArray;
 import org.json.JSONException;
