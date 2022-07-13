cf06925da2121b237b9ed900c88ec1a2796df487
Peter Na <ruikangna0605@hotmail.com>
2022-07-13 04:19:56 -0700

    1/3 chat done

--- app/src/main/java/com/example/help_m5/ChatActivity.java
+++ app/src/main/java/com/example/help_m5/ChatActivity.java
@@ -1,20 +1,48 @@
 package com.example.help_m5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ChatActivity extends AppCompatActivity {
 
     private String name;
     private String email;
+    private RecyclerView messagesRecyclerView;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://cpen321help-default-rtdb.firebaseio.com/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        messagesRecyclerView = findViewById(R.id.messagesRecyclerView);
+
         name = "Peter Na";
         email = "lufei8351@gmail.com";
+
+        messagesRecyclerView.setHasFixedSize(true);
+        messagesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
